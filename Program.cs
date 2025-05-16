6aaf4d965afb382406c732679452668e3ea59270
EnderEzhik <Achernov2007@gmail.com>
2025-05-17 00:10:44 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+namespace EzhikLoader.Server
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
